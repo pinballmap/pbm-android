91786398d2c2a51b4fe858f80443feaa3db18bfd
RyanTG <gratzer@posteo.de>
2016-10-04 12:18:35 -0700

    deleted unused import statements on about java

--- app/src/main/java/com/pbm/About.java
+++ app/src/main/java/com/pbm/About.java
@@ -1,11 +1,8 @@
 package com.pbm;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class About extends PinballMapActivity {
