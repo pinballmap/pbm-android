924efe0ba80ff278ec4f5e08b773565524e0e43b
RyanTG <gratzer@posteo.de>
2016-10-07 16:12:03 -0700

    fixed scroll issue on profile page

--- app/src/main/java/com/pbm/NonScrollListView.java
+++ app/src/main/java/com/pbm/NonScrollListView.java
@@ -3,26 +3,28 @@ package com.pbm;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
-public class NonScrollListView extends ListView {
-    private android.view.ViewGroup.LayoutParams params;
-    private int old_count = 0;
 
+
+public class NonScrollListView extends ListView {
+
+    public NonScrollListView(Context context) {
+        super(context);
+    }
     public NonScrollListView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-
+    public NonScrollListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
     @Override
-    protected void onDraw(Canvas canvas) {
-        if (getCount() != old_count) {
-            old_count = getCount();
-            params = getLayoutParams();
-
-            params.height = getCount() * (old_count > 0 ? getChildAt(0).getHeight() : 0);
-            setLayoutParams(params);
-        }
-
-        super.onDraw(canvas);
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightMeasureSpec_custom = MeasureSpec.makeMeasureSpec(
+                Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec_custom);
+        ViewGroup.LayoutParams params = getLayoutParams();
+        params.height = getMeasuredHeight();
     }
 }
