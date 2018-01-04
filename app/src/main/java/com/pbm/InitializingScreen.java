3b09cfffe2e17716e7543f2dcd5f85f712504951
Scott Wainstock <scott.wainstock@gmail.com>
2018-01-03 20:30:33 -0800

    revert old splash screen logic ; add loading splash screen

--- app/src/main/java/com/pbm/InitializingScreen.java
+++ app/src/main/java/com/pbm/InitializingScreen.java
@@ -14,7 +14,6 @@ public class InitializingScreen extends PinballMapActivity {
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.initializing_screen);
 	}
 
 	@Override
